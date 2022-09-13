5ba19158d8006511d323efdf9ca0dfa1e3c51077
shihad <bshihad@gmail.com>
2022-09-14 01:31:44 +0530

    first commit

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ app.listen(5000, () => {
 });
 
 mongoose
-  .connect("mongodb://localhost:27017/Notes", {
+  .connect("mongodb+srv://Shihadbh:Shihadbh964527@cluster0.uwsu7nh.mongodb.net/stickyNotes", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
