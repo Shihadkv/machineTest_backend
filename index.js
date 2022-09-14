717c80903ffcd977acfa5b03dc2c47386ecfc7c4
shihad <bshihad@gmail.com>
2022-09-14 16:37:36 +0530

    last commit

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ import authRoute from "./Routes/UserRoutes.js";
 import NotesRoutes from "./Routes/ContentRoutes.js"
 import bodyParser from "body-parser";
 import dotenv from "dotenv";
-// import cors from 'cors'
+import cors from 'cors'
 
 
 dotenv.config()
@@ -15,15 +15,24 @@ app.use(express.json());
 
 
 app.use(express.urlencoded({ extended: true }));
-
+app.use(bodyParser.json())
 app.use(cookieParser());
-// app.use(cors())
+app.use(cors())
+
 app.get("/",(req,res)=>res.send("api is working"))
 
 
 
 app.use("/api/auth", authRoute);
 app.use("/api/notes", NotesRoutes);
+app.use((req, res, next) => {
+  res.set("Cache-Control", "no-store");
+  next();
+});
+app.use((err,req,res,next)=>{
+  res.status(500).json(err)
+})
+
 
 const PORT = process.env.PORT || 5000
 
@@ -32,9 +41,8 @@ app.listen(PORT, () => {
 });
 
 
-console.log(process.env.MONGO_URL,"tftfv");
 mongoose
-  .connect("mongodb://localhost:27017/Notes", {
+  .connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
@@ -42,17 +50,5 @@ mongoose
     console.log("DataBase Connection Successful");
   })
   .catch((err) => {
-    //console.log(err);
     console.log(err.message, "DataBase Connection Failed");
   });
-
-app.use((req, res, next) => {
-  res.set("Cache-Control", "no-store");
-  next();
-});
-
-
-app.use((err,req,res,next)=>{
-    res.status(500).json(err)
-})
-
